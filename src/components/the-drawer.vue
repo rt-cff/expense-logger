38157f50653ae1cfcfa06be942c2651bc2225b13
Raymond Tam <raymondtamtwtp@gmail.com>
2020-10-08 01:49:20 +0800

    feat(drawer): add menu items & icons

--- src/components/the-drawer.vue
+++ src/components/the-drawer.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="drawer-container" @click="toggleDrawer">
     <div class="drawer-content" @click.stop="">
-      Drawer
+      <div><shopping-cart-icon />Expense</div>
+      <div><dollar-sign-icon />Income</div>
+      <div><bar-chart-2-icon />Summary</div>
+      <div><settings-icon />Setting</div>
     </div>
   </div>
 </template>
 
 <script>
+import DollarSignIcon from '../icons/dollar-sign.vue'
+import ShoppingCartIcon from '../icons/shopping-cart.vue'
+import BarChart2Icon from '../icons/bar-chart-2.vue'
+import SettingsIcon from '../icons/settings.vue'
 
 export default {
   name: 'Drawer',
+  components: {
+    DollarSignIcon,
+    ShoppingCartIcon,
+    BarChart2Icon,
+    SettingsIcon,
+  },
   props: {
     toggleDrawer: Function
   },
@@ -33,5 +46,18 @@ export default {
   width: 60vw;
   height: 100vh;
   background: lightyellow;
+}
+
+.drawer-content > * {
+  height: 3em;
+  padding: 1em;
+  text-align: left;
+  border-bottom: 0.5px solid lightgrey;
+}
+
+.drawer-content svg {
+  height: 1em;
+  width: 1em;
+  margin-right: 7px;
 }
 </style>
